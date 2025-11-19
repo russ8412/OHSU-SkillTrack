9f6378e5f14ef29ea2c3f59a1651be05f2104d3a
Aaron-40 <140855821+Aaron-40@users.noreply.github.com>
2025-11-18 16:48:29 -0800

    add DB table and give lambda function access to it

--- Backend/SkillTrackBackend/hello_world/app.py
+++ Backend/SkillTrackBackend/hello_world/app.py
@@ -2,6 +2,12 @@ import json
 
 # import requests
 
+import os
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["TABLE_NAME"])
+
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -33,10 +39,65 @@ def lambda_handler(event, context):
 
     #     raise e
 
+
+
+    ##here let's attempt to gtab stuff rom the table!
+    ########################
+    try:
+        # Fetch an item by primary key (HASH key)
+        response = table.get_item(
+            Key={
+                "ID": "User100"   # <-- Replace with the actual key value you want
+            }
+        )
+
+        item = response.get("Item")
+        statusCode = 0
+        body =       ""
+
+        if not item:
+            statusCode = 404
+            body = "Item not found"
+        else:
+            statusCode = 200
+            body = str(item)
+
+
+    except Exception as e:
+        print("Error fetching item:", e)
+        
+        statusCode = 500
+        body = "Internal Server Error"
+
+
+    ##########################
+
+
+    return {
+        "statusCode": statusCode,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type,Authorization",
+            "Access-Control-Allow-Methods": "GET,OPTIONS"
+        },       
+        "body": json.dumps({
+            "message": body,
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
+
+
+
+
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type,Authorization",
+            "Access-Control-Allow-Methods": "GET,OPTIONS"
+        },       
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello world!",
             # "location": ip.text.replace("\n", "")
         }),
     }
