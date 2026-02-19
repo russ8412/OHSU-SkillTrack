44310244b24255095a86e03973e4ea99347a96f9
Aaron-40 <140855821+Aaron-40@users.noreply.github.com>
2026-02-19 01:09:54 -0800

    update FetchUserData so teachers can get info... about particular students

--- Backend/SkillTrackBackend/FetchUserData/app.py
+++ Backend/SkillTrackBackend/FetchUserData/app.py
@@ -2,6 +2,8 @@
 /FetchUserData Function definition
 
 GET, not inputs besides authorization required
+optional input of array of student emails, in which case this function will switch modes and actually fetch data for student(s) rather than the calling user 
+Any teacher can currently call thi
 
 This fetches the row of data for the user is logged in to the app. This endpoint is not (at least currently) meant to return the data of other users
 '''
@@ -30,9 +32,6 @@ def normalize_string_sets(item):
 
 #This function is the main logic
 def lambda_handler(event, context):
-
-
-
     claims = event["requestContext"]["authorizer"]["claims"]
     email = claims.get("email")
     
@@ -50,47 +49,47 @@ def lambda_handler(event, context):
         
         item = response.get("Item")
 
-        #If the item is NOT found that means that we don't have a row for this user yet, let's generate one now
-        #This is a temporary measure just so we have data in place, later this will be elimated
-        if not item:            
-            try:
-                item ={
-                    "ID": "USER#"+email,
-                    "FirstName": None,
-                    "LastName": None,
-                    "Roles": {"Student"},
-                    "Courses": {
-                        "NRS-210-2020":{
-                            "CourseName": "NRS 210: Foundations of Nursing Health Promotion",
-                            "Skills":{
-                                "Handwashing - Infection Prevention": True,
-                                "PPE - Infection Prevention": False                                        
-                            }
-                        },
-                        "NRS-230-2020":{
-                            "CourseName":  "NRS 230: Pharmacology I",
-                            "Skills": {"IM - Medical Admin &...": False, "IV- Medical Admin...": True}
-                        }
-                    }
-                }
-                table.put_item(Item =item)
-                statusCode = 201
-                body = item
-            
-            except Exception as e:
-                statusCode = 500
-                body = "Data for user not found, and failed to create new data entry for user."
-        #case where the item is found, we simply return sucessful status code, and fetched data
-        else:
+        user_roles = item.get("Roles", [])
+        params = event.get("multiValueQueryStringParameters") or {}
+        studentEmails = params.get("Student_Emails", [])
+
+
+        #This is the default fetch call, which we just return the data of the user that made this call
+        if not studentEmails:
             statusCode = 200
 
             #python does not like the string set for some reason when calling json.dumps(), so use this function to turn any string sets as a list.      
             normalized_item = normalize_string_sets(item)            
             body = normalized_item
-    except:
-        #reach this except if we failed to find item and failed to create one
+        #otherwise, if there are student emails passed in we will try to obtain the student data for those students.
+        else:
+            if (not ("Admin" in user_roles) and not ("Teacher" in user_roles)):
+                statusCode = 403
+                output_body = "Error: You do not have permission to view details for other users "
+                return{
+                    "statusCode": statusCode,
+                    "headers": GlobalHeaders,       
+                    "body": json.dumps(output_body)
+                }
+            
+            #if we have permission, construct a list
+            studentsData = {}
+            for sEmail in studentEmails:
+                currStudent = table.get_item(Key= {"ID": "USER#" + sEmail}).get("Item")
+
+                if not currStudent:
+                    continue    #this student not found, so skil plen
+
+                currStudent = normalize_string_sets(currStudent)
+                studentsData[sEmail] = currStudent
+            
+            statusCode = 200
+            body = studentsData
+
+    #exception triggered, typically if our attempt to read from the databse goes wrong for some reason.
+    except Exception as e:
         statusCode = 500
-        body = "Failed to read data from the database"
+        body = "Error reading data from the database. Ended with this error: " + str(e)
     
     #return code based on what happened.
     return {
